f2703fedee3a98e0bad5ad06134db96a2b8403ec
cuongnt83it <cuongnt83@gmail.com>
2023-11-11 22:55:22 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        List<EmployeeModel> listEmployee = new List<EmployeeModel>() {
+        new EmployeeModel()
+        {
+            Id = 1,
+            Name = "Nguyễn Văn A",
+            Salary = 30000,
+            Status = true
+        },
+        new EmployeeModel()
+        {
+            Id = 2,
+            Name = "Nguyễn Văn A",
+            Salary = 30000,
+            Status = true
+        },
+            new EmployeeModel()
+            {
+            Id = 3,
+            Name = "Nguyễn Văn B",
+            Salary = 20000,
+            Status = false
+        },
+        new EmployeeModel()
+        {
+            Id = 4,
+            Name = "Nguyễn Văn C",
+            Salary = 30006,
+            Status = true
+        },
+        new EmployeeModel()
+        {
+            Id = 4,
+            Name = "Nguyễn Văn D",
+            Salary = 355000,
+            Status = false
+        },
+        new EmployeeModel()
+        {
+            Id = 6,
+            Name = "Nguyễn Văn E",
+            Salary = 366660,
+            Status = true
+        }};
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public JsonResult LoadData()
+        {
+           
+            return Json(new
+            {
+                data = listEmployee,
+                status = true
+            });
+        }
+        [HttpPost]
+        public JsonResult Update(string model)
+        {
+           EmployeeModel employee = JsonConvert.DeserializeObject<EmployeeModel>(model);
+            //save
+            EmployeeModel entity = listEmployee.Single(x => x.Id == employee.Id);
+            entity.Salary = employee.Salary;
+
+            return Json(new
+            {
+                status = true
+            });
+        }
+    }
+}
---
+++ wwwroot/js/controller/homeController.js
@@ -0,0 +1,63 @@
+﻿var homeController = {
+    init: function () {
+       
+        homeController.loadData();
+        homeController.registerEvent();
+    },
+    registerEvent: function () {
+        $('.txtSalary').off('keypress').on('keypress', function (e) {
+            if (e.which == 13) {
+                var id = $(this).data("id");
+                var value = $(this).val();
+                homeController.updateSalary(id, value);
+            }
+        });
+    },
+    updateSalary: function (id, value) {
+        var data = {
+            ID: id,
+            Salary: value
+        };
+        $.ajax({
+            url: 'Home/Update',
+            type: 'POST',
+            dataType: 'json',
+            data: { model: JSON.stringify(data)},
+            success: function (response) {
+                if (response.status) {
+                    alert('Update successed');
+                } else {
+                    alert('Update failed!');
+                }
+            }
+        })
+    },
+
+    loadData: function () {
+      
+        $.ajax({
+            url: 'Home/LoadData',
+            type: 'GET',
+            dataType: 'Json',
+            success: function (response) {
+                if (response.status) {
+                    var data = response.data;
+                    var html = '';
+                    var template = $('#data-template').html();
+                    Mustache.parse(template);
+                    $.each(data, function (i, item) {
+                        html += Mustache.render(template, {
+                            ID: item.id,
+                            Name: item.name,
+                            Salary: item.salary,
+                            Status: item.status
+                        });
+                    });
+                    $('#tblData').html(html);
+                    homeController.registerEvent();
+                }
+            }
+        })
+    }
+}
+homeController.init();
